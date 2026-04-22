bdd3ea4b6b278ac96e3cf8bc1e5feccd452e4a1f
Cecil Mathew <cecilsm901@gmail.com>
2026-04-22 08:03:18 +0530

    docs: rewrite README with full feature documentation

--- build_v140.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d C:\Users\cecil\motopulse
-call C:\flutter\bin\flutter.bat pub get > build_v140_log.txt 2>&1
-call C:\flutter\bin\flutter.bat build apk --release --target-platform android-arm64 >> build_v140_log.txt 2>&1
-echo BUILD_EXIT_CODE=%ERRORLEVEL% >> build_v140_log.txt
